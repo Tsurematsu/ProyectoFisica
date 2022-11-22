0ffb9236bdfc9a3b0ddcbb74082e89e5fb256615
MartinMedrano17 <martin.eduardo.mm@hotmail.com>
2022-11-22 09:30:04 -0500

    .

--- Interfaces/Opc3.java
+++ Interfaces/Opc3.java
@@ -1,14 +1,71 @@
 package Interfaces;
 
+import java.util.Scanner;
+import java.util.function.DoublePredicate;
+
 import AppData.Methods;
 
 public class Opc3 extends Methods {
 
-    public static Runnable Open = Methods.Set_Panel(new Opc3(), ()->Main(),()->loop());
     
-    static void Main(){
+    
+    public static void main(String[] args) {
+        
+    
+    Scanner scan = new Scanner(System.in);
 
+    System.out.println("Seleccione 1 si maneja datos en microcoulomb y Metros.");
+    System.out.println("Seleccione 2 si maneja datos en Nanocoulomb y Metros.");
+    System.out.println("Seleccione 3 si maneja datos en microcoulomb y Centimetros.");
+    System.out.println("Seleccione 4 si maneja datos en Nanocoulomb y Centimetros.");
+
+    int opcion = scan.nextInt();
+
+    switch (opcion){
+
+        case 1:
+
+            System.out.println("Ingrese el valor de la carga");
+            double Q = scan.nextInt();
+            System.out.println("Ingrese el valor de la distancia");
+            double R = scan.nextInt();
+
+            double microcoulomb = Math.pow(10 , -6);
+            double distancia = Math.pow(R,2);
+            double campoElectrico = (Q * microcoulomb)/distancia; 
+
+            System.out.println(campoElectrico + " Voltios");
+
+        break;
+        
+        case 2:
+            
+            System.out.println("Ingrese el valor de la carga");
+            double QN = scan.nextInt();
+            System.out.println("Ingrese el valor de la distancia");
+            double RN = scan.nextInt();
+
+            double nanocoulomb = Math.pow(10 , -9);
+            double distanciaN = Math.pow(RN,2);
+            double campoElectricoN = (QN * nanocoulomb)/distanciaN; 
+
+            System.out.println(campoElectricoN + " Voltios");
+            
+        break;
+        
+        case 3:
+            
+            
+        break;
+        
+        case 4:
+        
+        break;
     }
+ 
+}
+
+
     static void loop(){
 
     }
