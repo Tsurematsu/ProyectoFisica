6e94d575299585f252cfeba2ed4cac76bbfed5ae
-A L D A I R- <aldairgguer@gmail.com>
2022-11-26 17:29:49 -0500

    uu

--- Interfaces/Opc4.java
+++ Interfaces/Opc4.java
@@ -60,7 +60,6 @@ public class Opc4 extends Methods {
         panel1.$_RedondeoButtons=4;
         panel1.$$_Return_Msg_Correct="¡RESPUESTA CORRECTA!\n"+ res + " F";
         panel1.resultado=numpt/Voltaje;
-        panel1.$$_Return_Msg_Error="Respuesta incorrecta";
         panel1.$$_Numer_Button_Option=3;
         panel1.$_Relevar_Respuestas = true;
         panel1.$_JopcionMessage_Incorrect =true;
